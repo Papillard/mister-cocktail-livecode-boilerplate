65eb68ee9903b9d2b57a25c37fd42171cecdddf8
Edward Schults <edward.schults@gmail.com>
2016-08-12 15:13:49 +0200

    removed doses table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,23 +23,9 @@ ActiveRecord::Schema.define(version: 20160512154922) do
     t.string   "picture"
   end
 
-  create_table "doses", force: :cascade do |t|
-    t.string   "description"
-    t.integer  "cocktail_id"
-    t.integer  "ingredient_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "doses", ["cocktail_id"], name: "index_doses_on_cocktail_id", using: :btree
-  add_index "doses", ["ingredient_id"], name: "index_doses_on_ingredient_id", using: :btree
-
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_foreign_key "doses", "cocktails"
-  add_foreign_key "doses", "ingredients"
 end
