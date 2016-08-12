12c658b46d9ff1dfa348009716fa34e46cc89cf4
Edward Schults <edward.schults@gmail.com>
2016-08-12 15:18:12 +0200

    migrated db with cocktails and ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512154922) do
+ActiveRecord::Schema.define(version: 20150820151507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,4 +28,5 @@ ActiveRecord::Schema.define(version: 20160512154922) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
